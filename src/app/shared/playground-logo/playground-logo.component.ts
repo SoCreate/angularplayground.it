2959b19fea35c641797ed0dd60fe4efddfb4a8ed
brian treese <brian@socreate.it>
2017-01-27 13:49:27 -0800

    add style for the shell of the app

---
+++ src/app/shared/playground-logo/playground-logo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'playground-logo',
+  templateUrl: './playground-logo.component.html',
+  styleUrls: ['./playground-logo.component.scss']
+})
+export class PlaygroundLogoComponent {
+}
---
+++ src/app/shared/socreate-logo/socreate-logo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'socreate-logo',
+  templateUrl: './socreate-logo.component.html',
+  styleUrls: ['./socreate-logo.component.scss']
+})
+export class SoCreateLogoComponent {
+}
