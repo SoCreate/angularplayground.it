d969ebe91bd1224e3c09fb0c0532e68d94b60910
brian treese <brian@socreate.it>
2017-11-13 15:38:02 -0800

    update home page slider images for new keyboard shortcuts

--- src/app/home/slide-show/slide-show.component.ts
+++ src/app/home/slide-show/slide-show.component.ts
@@ -10,192 +10,187 @@ export class SlideShowComponent {
     {
       src: '/assets/slider/01.png',
       duration: '3500',
-      alt: ''
+      alt: 'Playin\' with the Playground'
     },
     {
       src: '/assets/slider/02.png',
       duration: '1000',
-      alt: ''
+      alt: 'Playground home screen'
     },
     {
       src: '/assets/slider/03.png',
-      duration: '1000',
-      alt: ''
+      duration: '2000',
+      alt: 'Playground command bar opened'
     },
     {
       src: '/assets/slider/04.png',
       duration: '2000',
-      alt: ''
+      alt: 'Person bio component default scenario selected'
     },
     {
       src: '/assets/slider/05.png',
-      duration: '2000',
-      alt: ''
+      duration: '1000',
+      alt: 'Person bio component sandbox default scenario isolation view'
     },
     {
       src: '/assets/slider/06.png',
       duration: '1000',
-      alt: ''
+      alt: 'Person bio component sandbox code'
     },
     {
       src: '/assets/slider/07.png',
-      duration: '1000',
-      alt: ''
+      duration: '2000',
+      alt: 'Person bio component sandbox code, default scenario highlighted'
     },
     {
       src: '/assets/slider/08.png',
-      duration: '2000',
-      alt: ''
+      duration: '1000',
+      alt: 'Person bio component sandbox code hidden'
     },
     {
       src: '/assets/slider/09.png',
       duration: '1000',
-      alt: ''
+      alt: 'Playground command bar opened'
     },
     {
       src: '/assets/slider/10.png',
-      duration: '1000',
-      alt: ''
+      duration: '2000',
+      alt: 'Person bio component simple scenario highlighted'
     },
     {
       src: '/assets/slider/11.png',
-      duration: '2000',
-      alt: ''
+      duration: '1000',
+      alt: 'Person bio component sandbox simple scenario isolation view'
     },
     {
       src: '/assets/slider/12.png',
       duration: '1000',
-      alt: ''
+      alt: 'Person bio component sandbox code'
     },
     {
       src: '/assets/slider/13.png',
-      duration: '1000',
-      alt: ''
+      duration: '2000',
+      alt: 'Person bio component sandbox code, simple scenario highlighted'
     },
     {
       src: '/assets/slider/14.png',
-      duration: '2000',
-      alt: ''
+      duration: '500',
+      alt: 'Person bio component sandbox code hidden'
     },
     {
       src: '/assets/slider/15.png',
-      duration: '500',
-      alt: ''
+      duration: '1000',
+      alt: 'Playground command bar opened'
     },
     {
       src: '/assets/slider/16.png',
-      duration: '1000',
-      alt: ''
+      duration: '500',
+      alt: 'Person bio component vertical scenario highlighted'
     },
     {
       src: '/assets/slider/17.png',
       duration: '500',
-      alt: ''
+      alt: 'Person bio component gray scenario highlighted'
     },
     {
       src: '/assets/slider/18.png',
-      duration: '500',
-      alt: ''
+      duration: '1000',
+      alt: 'Person bio component sandbox gray scenario isolation view'
     },
     {
       src: '/assets/slider/19.png',
       duration: '1000',
-      alt: ''
+      alt: 'Person bio component sandbox code'
     },
     {
       src: '/assets/slider/20.png',
-      duration: '1000',
-      alt: ''
+      duration: '2000',
+      alt: 'Person bio component sandbox code, gray scenario highlighted'
     },
     {
       src: '/assets/slider/21.png',
-      duration: '2000',
-      alt: ''
+      duration: '500',
+      alt: 'Person bio component sandbox code'
     },
     {
       src: '/assets/slider/22.png',
-      duration: '500',
-      alt: ''
+      duration: '1000',
+      alt: 'Person bio component template code'
     },
     {
       src: '/assets/slider/23.png',
-      duration: '1000',
-      alt: ''
+      duration: '200',
+      alt: 'Person bio component template code edited'
     },
     {
       src: '/assets/slider/24.png',
       duration: '200',
-      alt: ''
+      alt: 'Person bio component template code edited'
     },
     {
       src: '/assets/slider/25.png',
-      duration: '200',
-      alt: ''
+      duration: '1000',
+      alt: 'Person bio component template code bio added'
     },
     {
       src: '/assets/slider/26.png',
       duration: '1000',
-      alt: ''
+      alt: 'Playground reloading after save'
     },
     {
       src: '/assets/slider/27.png',
       duration: '1000',
-      alt: ''
+      alt: 'Person bio component scenario example updated'
     },
     {
       src: '/assets/slider/28.png',
-      duration: '1000',
-      alt: ''
+      duration: '2000',
+      alt: 'Person bio component scenario example bio highlighted'
     },
     {
       src: '/assets/slider/29.png',
-      duration: '2000',
-      alt: ''
+      duration: '500',
+      alt: 'Person bio component sandbox code hidden'
     },
     {
       src: '/assets/slider/30.png',
       duration: '500',
-      alt: ''
+      alt: 'Person bio component scenario example bio un-highlighted'
     },
     {
       src: '/assets/slider/31.png',
-      duration: '500',
-      alt: ''
+      duration: '1000',
+      alt: 'Playground command bar opened'
     },
     {
       src: '/assets/slider/32.png',
-      duration: '1000',
-      alt: ''
+      duration: '500',
+      alt: 'Person bio component vertical scenario highlighted'
     },
     {
       src: '/assets/slider/33.png',
       duration: '500',
-      alt: ''
+      alt: 'Person bio component simple scenario highlighted'
     },
     {
       src: '/assets/slider/34.png',
       duration: '500',
-      alt: ''
+      alt: 'Person bio component default scenario highlighted'
     },
     {
       src: '/assets/slider/35.png',
-      duration: '500',
-      alt: ''
+      duration: '1000',
+      alt: 'Person bio component sandbox default scenario isolation view'
     },
     {
       src: '/assets/slider/36.png',
-      duration: '1000',
-      alt: ''
+      duration: '2000',
+      alt: 'Person bio component scenario example bio highlighted'
     },
     {
       src: '/assets/slider/37.png',
-      duration: '2000',
-      alt: ''
-    },
-    {
-      src: '/assets/slider/38.png',
       duration: '1000',
-      alt: ''
+      alt: 'Person bio component scenario example bio un-highlighted'
     }
   ];
 
