b111ce371c935fe7a15b32156c886609ed59e9b3
mgmarlow <Graham.Marlow@socreate.it>
2018-01-11 14:45:24 -0800

    add docs for base href solution

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
+# Use // to get around git for windows placing C:\ProgramFiles\Git\ as resolved base href
 ng build --prod --output-path docs --base-href //
-git checkout -- docs/CNAME
-cp docs/index.html docs/404.html
+git checkout -- ./docs/CNAME
+cp docs/index.html ./docs/404.html
