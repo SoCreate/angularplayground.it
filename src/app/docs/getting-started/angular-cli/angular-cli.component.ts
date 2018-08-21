539c2a981724900cb5347cefee73963f498dde8b
Timothy Stoddard <tim.stoddard2@gmail.com>
2018-08-21 16:35:29 -0700

    simplify the docs regarding the installation process of playground

--- src/app/docs/getting-started/angular-cli/angular-cli.component.ts
+++ src/app/docs/getting-started/angular-cli/angular-cli.component.ts
@@ -1,23 +1,33 @@
 import { Component, OnInit, AfterViewChecked } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+const SHOW_MORE_FRAGMENT = 'show-more';
+
 @Component({
   selector: 'app-angular-cli',
   templateUrl: './angular-cli.component.html'
 })
 export class AngularCliComponent implements OnInit, AfterViewChecked {
+  showingExtraContent = false;
   private fragment: string;
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     window.scrollTo(0, 0);
-    this.route.fragment.subscribe(fragment => { this.fragment = fragment; });
+    this.route.fragment.subscribe(fragment => {
+      this.fragment = fragment;
+      this.showingExtraContent = this.fragment === SHOW_MORE_FRAGMENT;
+    });
   }
 
   ngAfterViewChecked(): void {
     try {
-      document.querySelector('#' + this.fragment).scrollIntoView();
-    } catch (e) { }
+      document.querySelector(`#${this.fragment}`).scrollIntoView();
+    } catch (e) {}
   }
 
+  getFragment() {
+    return this.showingExtraContent ? '' : SHOW_MORE_FRAGMENT;
+  }
 }
